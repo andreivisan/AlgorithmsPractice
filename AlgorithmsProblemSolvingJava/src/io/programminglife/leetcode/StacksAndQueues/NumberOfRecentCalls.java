382fbda49c9a57781e27a9ab5dcb6814680c1eea
Andrei Visan <andreivisan@andreis-mbp.home>
2020-09-07 12:59:31 +0200

    Leetcode 933. Number of Recent Calls: https://leetcode.com/problems/number-of-recent-calls/

---
+++ AlgorithmsProblemSolvingJava/src/io/programminglife/leetcode/StacksAndQueues/NumberOfRecentCalls.java
@@ -0,0 +1,24 @@
+package io.programminglife.leetcode.StacksAndQueues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NumberOfRecentCalls {
+
+    Queue<Integer> calls;
+
+    public RecentCounter() {
+        calls = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        calls.add(t);
+
+        while(calls.peek() < t - 3000) {
+            calls.poll();
+        }
+
+        return calls.size();
+    }
+    
+}
