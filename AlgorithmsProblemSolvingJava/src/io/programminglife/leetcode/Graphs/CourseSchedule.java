d077f0061d73ef0e2431b689f411c8c26817f1a0
Andrei Visan <andrei.m.visan@gmail.com>
2020-11-15 14:51:26 +0100

    Leetcode 207. Course Schedule - Added topological sort

--- AlgorithmsProblemSolvingJava/src/io/programminglife/leetcode/Graphs/CourseSchedule.java
+++ AlgorithmsProblemSolvingJava/src/io/programminglife/leetcode/Graphs/CourseSchedule.java
@@ -8,22 +8,33 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 
 public class CourseSchedule {
+
+    Map<Integer, Integer> inDegree = new HashMap<>();
     
-    public static boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> courseDictionary = createCourseDictionary(prerequisites);
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses == 1 || prerequisites.length == 0) {
+            return true;
+        } 
 
         for (int i = 0; i < numCourses; i++) {
-            if (isCyclic(i, courseDictionary)) {
-                return false;
-            }
+            inDegree.put(i, 0);
         }
 
-        return true;
+        Map<Integer, List<Integer>> courseDictionary = createCourseDictionary(prerequisites);
+
+        // for (int i = 0; i < numCourses; i++) {
+        //     if (isCyclic(i, courseDictionary)) {
+        //         return false;
+        //     }
+        // }
+
+        return !isCyclicTopological(numCourses, courseDictionary);
     }
 
-    private static Map<Integer, List<Integer>> createCourseDictionary(int[][] prerequisites) {
+    private Map<Integer, List<Integer>> createCourseDictionary(int[][] prerequisites) {
         Map<Integer, List<Integer>> courseDictionary = new HashMap<>();
 
         for (int v = 0; v < prerequisites.length; v++) {
@@ -34,12 +45,13 @@ public class CourseSchedule {
                 adjList.add(prerequisites[v][0]);
                 courseDictionary.put(prerequisites[v][1], adjList);
             }
+            inDegree.put(prerequisites[v][0], inDegree.getOrDefault(prerequisites[v][0], 0) + 1);
         }
 
         return courseDictionary;
     }
 
-    private static boolean isCyclic(int startNode, Map<Integer, List<Integer>> courseDictionary) {
+    private boolean isCyclic(int startNode, Map<Integer, List<Integer>> courseDictionary) {
         Queue<Integer> queue = new LinkedList<>();
         Set<Integer> visited = new HashSet<>();
 
@@ -67,12 +79,40 @@ public class CourseSchedule {
         return false;
     }
 
+    private boolean isCyclicTopological(int n, Map<Integer, List<Integer>> courseDictionary) {
+        int count = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        for (Integer key : inDegree.keySet()) {
+            if (inDegree.get(key) == 0) {
+                stack.push(key);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            count++;
+
+            List<Integer> currentAdjList = courseDictionary.get(current);
+            if (currentAdjList != null) {
+                for (Integer adj : currentAdjList) {
+                    inDegree.put(adj, inDegree.get(adj) - 1);
+                    if (inDegree.get(adj) == 0) {
+                        stack.push(adj);
+                    }
+                }
+            }
+        }
+
+        return count != n;
+    }
+
     public static void main(String[] args) {
         int[][] prerequisites = {{1, 0}, {2, 1}, {2, 5}, {0, 3}, {4, 3}, {3, 5}, {4, 5}};
         int[][] prerequisites2 = {{1, 0}, {0, 1}};
         int[][] prerequisites3 = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
 
-        System.out.println(canFinish(2, prerequisites2));
+        System.out.println(new CourseSchedule().canFinish(4, prerequisites3));
     }
 
 }
