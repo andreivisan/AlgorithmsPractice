efcc9ab6632f937217a07464c248860ec357a975
Andrei Visan <andrei.m.visan@gmail.com>
2020-12-13 18:43:02 +0100

    Leetcode 746. Min Cost Climbing Stairs - DP

--- AlgorithmsProblemSolvingJava/src/io/programminglife/dynamicprogramming/MinimumCostOfClimbingStairs.java
+++ AlgorithmsProblemSolvingJava/src/io/programminglife/dynamicprogramming/MinimumCostOfClimbingStairs.java
@@ -20,10 +20,37 @@ public class MinimumCostOfClimbingStairs {
         return cost[i] + Math.min(minCostBrute(i - 1, cost), minCostBrute(i - 2, cost));
     }
 
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] mins = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            mins[i] = -1;
+        }
+
+        return Math.min(minCost(n - 1, cost, mins), minCost(n - 2, cost, mins));
+    }
+
+    public int minCost(int i, int[] cost, int[] mins) {
+        if (i < 0) {
+            return 0;
+        }
+
+        if (i <= 1) {
+            return cost[i];
+        }
+
+        if (mins[i] == -1) {
+            mins[i] = cost[i] + Math.min(minCost(i - 1, cost, mins), minCost(i - 2, cost, mins));
+        }
+
+        return mins[i];
+    }
+
     public static void main(String[] args) {
         int[] cost = {10, 15, 20};
 
-        System.out.println(new MinimumCostOfClimbingStairs().minCostClimbingStairsBrute(cost));
+        System.out.println(new MinimumCostOfClimbingStairs().minCostClimbingStairs(cost));
     }
 
 }
