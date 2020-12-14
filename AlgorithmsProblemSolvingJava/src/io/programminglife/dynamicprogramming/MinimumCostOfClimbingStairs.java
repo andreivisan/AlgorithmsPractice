623e7af69a6a366c4af04cfc1a61157e3e1f07a8
Andrei Visan <andrei.m.visan@gmail.com>
2020-12-14 14:33:31 +0100

    Leetcode 746. Min Cost Climbing Stairs - DP bottom up optimized

--- AlgorithmsProblemSolvingJava/src/io/programminglife/dynamicprogramming/MinimumCostOfClimbingStairs.java
+++ AlgorithmsProblemSolvingJava/src/io/programminglife/dynamicprogramming/MinimumCostOfClimbingStairs.java
@@ -47,7 +47,7 @@ public class MinimumCostOfClimbingStairs {
         return mins[i];
     }
 
-    public int minCostClimbingStairs(int[] cost) {
+    public int minCostClimbingStairsIter(int[] cost) {
         int n = cost.length;
         int minCost[] = new int[n];
         
@@ -60,6 +60,29 @@ public class MinimumCostOfClimbingStairs {
         }
 
         return Math.min(minCost[n - 1], minCost[n - 2]);
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return cost[0];
+        }
+
+        int last = cost[1];
+        int secondToLast = cost[0];
+        
+        for (int i = 2; i < n; i++) {
+            int current = cost[i] + Math.min(last, secondToLast);
+            secondToLast = last;
+            last = current;
+        }
+
+        return Math.min(last, secondToLast);
     }
 
     public static void main(String[] args) {
