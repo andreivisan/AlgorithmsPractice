f0fa59ea9043601f08ff9087e9761565e2d42c40
Andrei Visan <andrei.m.visan@gmail.com>
2020-12-16 13:14:46 +0100

    LeetCode 392. Is Subsequence

---
+++ AlgorithmsProblemSolvingJava/src/io/programminglife/leetcode/Easy/StringSubsequence.java
@@ -0,0 +1,23 @@
+package io.programminglife.leetcode.Easy;
+
+public class StringSubsequence {
+
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < t.length() && j < s.length()) {
+            if (t.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+
+            i++;
+        }
+
+        if (j == s.length()) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
