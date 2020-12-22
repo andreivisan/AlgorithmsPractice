d2d8829e292a0bdfa848020613f96469d1cfdae1
Andrei Visan <andrei.m.visan@gmail.com>
2020-12-22 13:45:48 +0100

    AlgoExpert - Product Sum

---
+++ AlgorithmsProblemSolvingJava/src/io/programminglife/algoexpert/easy/ProductSum.java
@@ -0,0 +1,54 @@
+package io.programminglife.algoexpert.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductSum {
+
+    public static int productSum(List<Object> array) {
+        return sum(array, 1);
+    }
+
+    private static int sum(List<Object> array, int multiplier) {
+        int pSum = 0;
+
+        for (Object el : array) {
+            if (el instanceof ArrayList) {
+                @SuppressWarnings("unchecked")
+                List<Object> embList = (ArrayList<Object>)el;
+                pSum += sum(embList, multiplier + 1);
+            } else {
+                pSum += (int)el;
+            }
+        }
+
+        return multiplier * pSum;
+    }
+
+    public static void main(String[] args) {
+        List<Object> mainInput = new ArrayList<>();
+        
+        List<Object> sublist1 = new ArrayList<>();
+        sublist1.add(7);
+        sublist1.add(-1);
+        
+        List<Object> sublist2 = new ArrayList<>();
+        
+        List<Object> sublist3 = new ArrayList<>();
+        sublist3.add(-13);
+        sublist3.add(8);
+        
+        sublist2.add(6);
+        sublist2.add(sublist3);
+        
+        mainInput.add(5);
+        mainInput.add(2);
+        mainInput.add(sublist1);
+        mainInput.add(3);
+        mainInput.add(sublist2);
+        mainInput.add(4);
+
+        System.out.println(productSum(mainInput));
+    }
+    
+}
