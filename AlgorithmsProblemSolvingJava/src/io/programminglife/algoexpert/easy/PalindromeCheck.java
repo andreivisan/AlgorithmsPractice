b09dcfbd6d218aa056751fdf7afbbe3dbee846df
Andrei Visan <andrei.m.visan@gmail.com>
2020-12-22 14:50:58 +0100

    AlgoExpert Palindrome Check

---
+++ AlgorithmsProblemSolvingJava/src/io/programminglife/algoexpert/easy/PalindromeCheck.java
@@ -0,0 +1,26 @@
+package io.programminglife.algoexpert.easy;
+
+public class PalindromeCheck {
+    
+    public static boolean isPalindrome(String str) {
+        int left = 0 , right = str.length() - 1;
+
+        while(left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "abcxcsa";
+
+        System.out.println(isPalindrome(input));
+    }
+
+}
--- AlgorithmsProblemSolvingJava/src/io/programminglife/algoexpert/easy/ProductSum.java
+++ AlgorithmsProblemSolvingJava/src/io/programminglife/algoexpert/easy/ProductSum.java
@@ -1,7 +1,9 @@
 package io.programminglife.algoexpert.easy;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ProductSum {
 
