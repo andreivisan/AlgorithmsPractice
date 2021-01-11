60dd6c11220cfb7d39586d186fcb58c848ea3731
Andrei Visan <andrei.visan@kpn.com>
2021-01-11 12:35:51 +0100

    AlgoExpert - BST Traversal Iterative

--- AlgorithmsProblemSolvingJava/src/io/programminglife/algoexpert/medium/BSTTraversal.java
+++ AlgorithmsProblemSolvingJava/src/io/programminglife/algoexpert/medium/BSTTraversal.java
@@ -1,6 +1,7 @@
 package io.programminglife.algoexpert.medium;
 
 import java.util.List;
+import java.util.Stack;
 
 public class BSTTraversal {
 
@@ -42,6 +43,69 @@ public class BSTTraversal {
         postOrderTraverse(tree.right, array);
 
         array.add(tree.value);
+
+        return array;
+    }
+
+    public static List<Integer> inOrderTraverseIterative(BST tree, List<Integer> array) {
+        Stack<BST> stack = new Stack<>();
+
+        BST current = tree;
+
+        while (!stack.isEmpty() || current != null) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                current = stack.pop();
+                array.add(current.value);
+                current = current.right;
+            }
+        }
+
+        return array;
+    }
+
+    public static List<Integer> preOrderTraverseIterative(BST tree, List<Integer> array) {
+        Stack<BST> stack = new Stack<>();
+
+        BST root = tree;
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            BST current = stack.pop();
+            array.add(current.value);
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+
+        return array;
+    }
+
+    public static List<Integer> postOrderTraverseIterative(BST tree, List<Integer> array) {
+        Stack<BST> stack = new Stack<>();
+
+        BST root = tree;
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            BST current = stack.pop();
+            array.add(0, current.value);
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+        }
 
         return array;
     }
